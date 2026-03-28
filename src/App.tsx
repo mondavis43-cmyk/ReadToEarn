7bd447662557e0f1a1a9e975a7d0e86721fce94c
mondavis43-cmyk <mondavis43@gmail.com>
2026-03-27 19:25:37 -0500

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,5 @@
 import { AuthProvider } from './contexts/AuthContext';
 import { Router } from './components/Router';
-import { NavBar } from './components/NavBar';
-import { RequestBook } from './pages/RequestBook';
-import { Refer } from './pages/Refer';
-import { Authors } from './pages/Authors';
-import { FAQ } from './pages/FAQ';
 
 function App() {
   return (
