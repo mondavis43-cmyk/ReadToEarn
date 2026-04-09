ee7a73eef9c6b6b051978f658a892481193ecc90
mondavis43-cmyk <mondavis43@gmail.com>
2026-04-08 21:22:09 -0500

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,7 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
-import { ThemeProvider } from './contexts/ThemeContext';
 import App from './App';
+import { ThemeProvider } from './context/ThemeContext';
 import './index.css';
 
 createRoot(document.getElementById('root')!).render(
