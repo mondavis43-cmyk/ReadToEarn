19027f7feca3db3054bf4bd57b7f1a4187a10d87
mondavis43-cmyk <mondavis43@gmail.com>
2026-04-18 15:28:26 -0500

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,11 @@
 import { AuthProvider } from './contexts/AuthContext';
 import { Router } from './components/Router';
+import { TermsOfService } from './pages/TermsOfService';
+import { PrivacyPolicy } from './pages/PrivacyPolicy';
+
+// inside your routes:
+<Route path="/terms" element={<TermsOfService />} />
+<Route path="/privacy" element={<PrivacyPolicy />} />
 
 function App() {
   return (
