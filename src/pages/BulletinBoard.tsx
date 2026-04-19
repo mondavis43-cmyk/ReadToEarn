b374fb8fa82fdf8620e1fe56ddbfe93d17256385
mondavis43-cmyk <mondavis43@gmail.com>
2026-04-19 02:59:45 -0500

    Update BulletinBoard.tsx

--- src/pages/BulletinBoard.tsx
+++ src/pages/BulletinBoard.tsx
@@ -1,5 +1,5 @@
 import { useState, useEffect } from 'react';
-import { supabase } from '../lib/supabaseClient';
+import { supabase } from '../lib/supabase';
 import { useTheme } from '../contexts/ThemeContext';
 
 interface BulletinBook {
